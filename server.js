51d3b3fc74b9db2038d18c467918a86919f8a90c
tommey <tommey@lightnet.ie>
2026-07-05 11:05:19 +0100

    update package json type

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const fetch = require("node-fetch");
+
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const fetch = require("node-fetch");
 
 const app = express();
 app.use(cors());
